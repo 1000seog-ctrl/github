518c5b9ee7f8cde521117ac250a6eb91277189a9
1000seog-ctrl <1000seog@gmail.com>
2025-12-27 21:38:35 +0900

    Create config.js
    Config file

---
+++ config.js
@@ -0,0 +1,11 @@
+window.LAUNCHER_CONFIG = {
+  title: "전국 봉사모임 (서울 종로지부)",
+  footer: "항상 감사합니다",
+  openMode: "newtab",
+  buttons: [
+    { label: "홈페이지", url: "https://example.com" },
+    { label: "방송국",   url: "https://example.org" },
+    { label: "유튜브1",  url: "https://youtube.com" },
+    { label: "유튜브2",  url: "https://youtube.com" }
+  ]
+};
